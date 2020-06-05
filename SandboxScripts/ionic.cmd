ac65988aace5244a5f22be99fbf6cb37aacb40da
Niels Hüsken <niels@monarch.de>
2020-06-05 14:54:27 +0200

    changed install order a bit

--- SandboxScripts/ionic.cmd
+++ SandboxScripts/ionic.cmd
@@ -1,17 +1,19 @@
 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install cmder -y
+
 REM Download VSCode
 curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output C:\users\WDAGUtilityAccount\Desktop\vscode.exe
  
 REM Install and run VSCode
 C:\users\WDAGUtilityAccount\Desktop\vscode.exe /verysilent /suppressmsgboxes
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install nodejs -y
 choco install android-sdk -y
 choco install git -y
 choco install gradle -y
-choco install cmder -y
 
 call refreshenv
 call code --install-extension angular-schule.angular-schule-extension-pack
