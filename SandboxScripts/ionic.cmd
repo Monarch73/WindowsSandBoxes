bcce742e90855be02f17229759a423f62009fed9
Niels Hüsken <niels@monarch.de>
2020-06-05 11:46:06 +0200

    debugging working

--- SandboxScripts/ionic.cmd
+++ SandboxScripts/ionic.cmd
@@ -10,13 +10,17 @@ C:\users\WDAGUtilityAccount\Desktop\vscode.exe /verysilent /suppressmsgboxes
 choco install nodejs -y
 choco install android-sdk -y
 choco install git -y
+choco install gradle -y
 
 call refreshenv
-
+call code --install-extension angular-schule.angular-schule-extension-pack
+call code --install-extension msjsdiag.cordova-tools
 call npm install -g @ionic/cli
+call npm install -g @angular/cli
 call npm install -g cordova
 call npm install -g cordova-res
 call npm install -g native-run
+call npm install -g yarn
 
 mkdir -p c:\users\WDAGUtilityAccount\.android\avd
 
