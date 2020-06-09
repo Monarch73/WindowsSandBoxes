00fbdc166294cdfaac9c99ef4efedb9edd3145f9
Niels Hüsken <monarch@hotmail.de>
2020-06-09 15:35:42 +0200

    added version numbers

--- SandboxScripts/ionic.cmd
+++ SandboxScripts/ionic.cmd
@@ -10,17 +10,14 @@ REM Install and run VSCode
 C:\users\WDAGUtilityAccount\Desktop\vscode.exe /verysilent /suppressmsgboxes
 
 
-choco install nodejs -y
-choco install android-sdk -y
-choco install git -y
-choco install gradle -y
+choco install nodejs android-sdk git gradle -y
 
 call refreshenv
-call npm install -g @ionic/cli
-call npm install -g @angular/cli
-call npm install -g cordova
-call npm install -g cordova-res
-call npm install -g native-run
+call npm install -g @angular/cli@7.3.10
+call npm install -g @ionic/cli@6.9.3
+call npm install -g cordova@9.0.0
+call npm install -g cordova-res@0.14.0
+call npm install -g native-run@0.2.9
 call npm install -g yarn
 
 mkdir -p c:\users\WDAGUtilityAccount\.android\avd
