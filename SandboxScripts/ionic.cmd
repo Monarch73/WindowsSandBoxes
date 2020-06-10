857e4ace9d0e6c4b09a2bcd5d7195e4b3b45f0cd
Niels Hüsken <monarch@hotmail.de>
2020-06-10 10:05:54 +0200

    added information

--- SandboxScripts/ionic.cmd
+++ SandboxScripts/ionic.cmd
@@ -10,7 +10,7 @@ REM Install and run VSCode
 C:\users\WDAGUtilityAccount\Desktop\vscode.exe /verysilent /suppressmsgboxes
 
 
-choco install nodejs android-sdk git gradle -y
+choco install nodejs android-sdk git gradle python2 -y
 
 call refreshenv
 call npm install -g @angular/cli@7.3.10
@@ -18,11 +18,20 @@ call npm install -g @ionic/cli@6.9.3
 call npm install -g cordova@9.0.0
 call npm install -g cordova-res@0.14.0
 call npm install -g native-run@0.2.9
+call npm install -g node-sass@4.14.1
 call npm install -g yarn
+call npm install -g windows-build-tools
+call npm install -g node-sass
 
 mkdir -p c:\users\WDAGUtilityAccount\.android\avd
 
 call sdkmanager --install build-tools;29.0.3
 call code --install-extension angular-schule.angular-schule-extension-pack
 call code --install-extension msjsdiag.cordova-tools
+
+rem todo:
+rem install python2
+rem install visual studio 2017 mit c++ desktop workload
+rem add path to msbuild bin-folder
+
 
